2d116ae88c24d2306ff08a5e7e942d6e14be5382
Prasad Ingole <10679929+prasadingole@users.noreply.github.com>
2022-05-18 22:52:43 +1000

    Added animation using useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,33 @@
-import { useContext } from 'react'
+import { useContext, useEffect , useState } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
 const HeaderCartButton = (props) => {
+    const[buttonHighLighted,setButtonIsHighlighted] =useState(false);
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
     const numberOfCartItems = cartCtx.items.reduce((currentNumber,item)=> {
         return currentNumber + item.amount;
     },0);
-    return (<button className={classes.button} onClick={props.onClick}>
+
+    const btnClasses = `${classes.button} ${buttonHighLighted ? classes.bump : ''}`;
+    useEffect(()=> {
+        if(items.length === 0){
+            return;
+        }
+        
+        setButtonIsHighlighted(true);
+        
+        const timer = setTimeout(()=> {
+            setButtonIsHighlighted(false);
+        },300);
+
+        return () => {
+           clearTimeout(timer);
+        };
+    },[items]);
+
+    return (<button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}><CartIcon /></span>
         <span>Your Cart</span>
         <span className={classes.badge}>{numberOfCartItems}</span>
